995fddbc7a0d80a35d9c219af07149cb29c7d2c2
Caiua vieira <caiua.stv.puccampinas@edu.br>
2025-04-15 15:57:57 -0300

    Botao ajustado e tela branca finalmente resolvida

--- app/src/main/java/com/example/superid2/DescricaoActivity.kt
+++ app/src/main/java/com/example/superid2/DescricaoActivity.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -32,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.superid.R
 
 import com.example.superid2.ui.theme.SuperID2Theme
@@ -44,13 +47,14 @@ class descricaoActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SuperID2Theme {
-                var mostrarDescricao by remember { mutableStateOf(true) }
+                val mostrarDescricao by remember { mutableStateOf(true) }
 
                 if (mostrarDescricao) {
                     ExibirCardDescricao(
                         mostrarDescricao = true,
                         aoFechar = {
-                            mostrarDescricao = false
+                            //Problema da tela branca abaixo
+                            //mostrarDescricao = false
                             startActivity(Intent(this@descricaoActivity, loginActivity::class.java))
                             finish()
                         }
