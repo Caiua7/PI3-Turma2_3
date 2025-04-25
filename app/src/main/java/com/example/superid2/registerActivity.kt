c65dd75a9c3e3f9c32282b7c15bbdc3365f93a8b
Caiua vieira <caiua.stv.puccampinas@edu.br>
2025-04-24 22:09:52 -0300

    Login funcionando corretamente, setinhas adicionadas em ambas telas

--- app/src/main/java/com/example/superid2/registerActivity.kt
+++ app/src/main/java/com/example/superid2/registerActivity.kt
@@ -42,6 +42,10 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import android.provider.Settings
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+
+import androidx.compose.material.icons.filled.ArrowBack
 
 
 class registerActivity : ComponentActivity() {
@@ -72,123 +76,170 @@ fun RegisterScreen(onRegisterSuccess: () -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(20.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Logo",
-            modifier = Modifier
-                .size(300.dp)
-                .padding(bottom = 10.dp)
-        )
+        // Setinha de voltar
+        val context = LocalContext.current
 
-        OutlinedTextField(
-            value = nome,
-            onValueChange = { nome = it },
-            label = { Text("Nome", color = Color.White) },
-            leadingIcon = { Icon(Icons.Rounded.Person, contentDescription = null) },
-            textStyle = TextStyle(color = Color.White),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
+        Spacer(modifier = Modifier.padding(top = 20.dp)) // descendo a seta um pouco
 
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email", color = Color.White) },
-            leadingIcon = { Icon(Icons.Rounded.Email, contentDescription = null) },
-            textStyle = TextStyle(color = Color.White),
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "Voltar",
+            tint = Color.White,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-
-        OutlinedTextField(
-            value = senha,
-            onValueChange = { senha = it },
-            label = { Text("Senha", color = Color.White) },
-            leadingIcon = { Icon(Icons.Rounded.Lock, contentDescription = null) },
-            textStyle = TextStyle(color = Color.White),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-
-        OutlinedTextField(
-            value = confirmarSenha,
-            onValueChange = { confirmarSenha = it },
-            label = { Text("Confirme a senha", color = Color.White) },
-            leadingIcon = { Icon(Icons.Rounded.Lock, contentDescription = null) },
-            textStyle = TextStyle(color = Color.White),
-            modifier = Modifier
-                .fillMaxWidth()
                 .padding(bottom = 16.dp)
+                .size(32.dp)
+                .clickable {
+                    val intent = Intent(context, loginActivity::class.java)
+                    context.startActivity(intent)
+                    if (context is Activity) {
+                        context.finish() // fecha a tela de cadastro
+                    }
+                }
         )
 
-        Button(
-            onClick = {
-                // Confirmação se as senhas sao iguais
-                if (senha != confirmarSenha) {
-                    Toast.makeText(context, "As senhas não coincidem.", Toast.LENGTH_SHORT).show()
-                } else if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
-                    Toast.makeText(context, "Preencha todos os campos.", Toast.LENGTH_SHORT).show()
-                } else {
-                    // Usando para criar uma conta no Auth (aqui tem requisito
-                    // de email e senha pelo menos 6 digitos)
-                    val auth = FirebaseAuth.getInstance()
-                    auth.createUserWithEmailAndPassword(email, senha)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                val uid = auth.currentUser?.uid
-                                val androidId = Settings.Secure.getString(
-                                    context.contentResolver,
-                                    Settings.Secure.ANDROID_ID
-                                )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(bottom = 10.dp)
+            )
 
-                                // Salva o Nome, email, uid e imei no firestore
-                                val userData = hashMapOf(
-                                    "nome" to nome,
-                                    "email" to email,
-                                    "uid" to uid,
-                                    "imei" to androidId
-                                )
+            OutlinedTextField(
+                value = nome,
+                onValueChange = { nome = it },
+                label = { Text("Nome", color = Color.White) },
+                leadingIcon = { Icon(Icons.Rounded.Person, contentDescription = null) },
+                textStyle = TextStyle(color = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
+            )
 
-                                val db = Firebase.firestore
-                                if (uid != null) {
-                                    db.collection("usuarios").document(uid)
-                                        .set(userData)
-                                        .addOnSuccessListener {
-                                            auth.currentUser?.sendEmailVerification()?.addOnCompleteListener { verificationTask -> //envia email de verificacao, e dps verifica
-                                                if (verificationTask.isSuccessful) {
-                                                    Toast.makeText(context, "Verifique seu e-mail para ativar sua conta.", Toast.LENGTH_LONG).show()
-                                                    val intent = Intent(context, EmailVerificationActivity::class.java)
-                                                    context.startActivity(intent)
-                                                    if (context is Activity) context.finish() //encerra a atividade que verification
-                                                } else {
-                                                    Toast.makeText(context, "Erro ao enviar email de verificação.", Toast.LENGTH_SHORT).show()
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email", color = Color.White) },
+                leadingIcon = { Icon(Icons.Rounded.Email, contentDescription = null) },
+                textStyle = TextStyle(color = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = senha,
+                onValueChange = { senha = it },
+                label = { Text("Senha", color = Color.White) },
+                leadingIcon = { Icon(Icons.Rounded.Lock, contentDescription = null) },
+                textStyle = TextStyle(color = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = confirmarSenha,
+                onValueChange = { confirmarSenha = it },
+                label = { Text("Confirme a senha", color = Color.White) },
+                leadingIcon = { Icon(Icons.Rounded.Lock, contentDescription = null) },
+                textStyle = TextStyle(color = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            Button(
+                onClick = {
+                    // Confirmação se as senhas sao iguais
+                    if (senha != confirmarSenha) {
+                        Toast.makeText(context, "As senhas não coincidem.", Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                        Toast.makeText(context, "Preencha todos os campos.", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        // Usando para criar uma conta no Auth (aqui tem requisito
+                        // de email e senha pelo menos 6 digitos)
+                        val auth = FirebaseAuth.getInstance()
+                        auth.createUserWithEmailAndPassword(email, senha)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    val uid = auth.currentUser?.uid
+                                    val androidId = Settings.Secure.getString(
+                                        context.contentResolver,
+                                        Settings.Secure.ANDROID_ID
+                                    )
+
+                                    // Salva o Nome, email, uid e imei no firestore
+                                    val userData = hashMapOf(
+                                        "nome" to nome,
+                                        "email" to email,
+                                        "uid" to uid,
+                                        "imei" to androidId
+                                    )
+
+                                    val db = Firebase.firestore
+                                    if (uid != null) {
+                                        db.collection("usuarios").document(uid)
+                                            .set(userData)
+                                            .addOnSuccessListener {
+                                                auth.currentUser?.sendEmailVerification()
+                                                    ?.addOnCompleteListener { verificationTask -> //envia email de verificacao, e dps verifica
+                                                        if (verificationTask.isSuccessful) {
+                                                            Toast.makeText(
+                                                                context,
+                                                                "Verifique seu e-mail para ativar sua conta.",
+                                                                Toast.LENGTH_LONG
+                                                            ).show()
+                                                            val intent = Intent(
+                                                                context,
+                                                                EmailVerificationActivity::class.java
+                                                            )
+                                                            context.startActivity(intent)
+                                                            if (context is Activity) context.finish() //encerra a atividade que verification
+                                                        } else {
+                                                            Toast.makeText(
+                                                                context,
+                                                                "Erro ao enviar email de verificação.",
+                                                                Toast.LENGTH_SHORT
+                                                            ).show()
+                                                        }
+                                                    }
+
+                                                if (context is Activity) {
+                                                    context.finish()
                                                 }
                                             }
-
-                                            if (context is Activity) {
-                                                context.finish()
+                                            .addOnFailureListener { e ->
+                                                Toast.makeText(
+                                                    context,
+                                                    "Erro ao salvar dados: ${e.message}",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
                                             }
-                                        }
-                                        .addOnFailureListener { e ->
-                                            Toast.makeText(context, "Erro ao salvar dados: ${e.message}", Toast.LENGTH_SHORT).show()
-                                        }
+                                    }
+                                } else {
+                                    Toast.makeText(
+                                        context,
+                                        "Erro ao criar conta: ${task.exception?.message}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                                 }
-                            } else {
-                                Toast.makeText(context, "Erro ao criar conta: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                             }
-                        }
-                }
-            },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1B5E20)),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Registrar")
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1B5E20)),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Registrar")
+            }
         }
     }
 }
