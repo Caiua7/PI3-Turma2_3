c672064a92d7ad30fc55213a95ce880e50433ac6
igor <igor.ribeiroc77@gmail.com>
2025-05-20 09:12:58 -0300

    Tentativa de chamar performAuth

---
+++ projeto3web/backend/src/server.ts
@@ -0,0 +1,50 @@
+import express from "express"
+import {Request, Response, Router} from "express";
+import cors from "cors"
+import path from "path";
+import dotenv from "dotenv"
+import fetch from "node-fetch"
+
+dotenv.config()
+const port = 3000;
+const routes = Router();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(routes);
+
+
+routes.post("/api/perform-auth", async (req: Request, res: Response) => {
+  const site = "www.cursini.com";
+  const apiKey = process.env.PARTNER_API_KEY;
+
+  try {
+    const response = await fetch("https://performauth-qlbxjdguza-rj.a.run.app", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ site, apiKey })
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error: any) {
+    console.error("Erro ao chamar performAuth:", error.message);
+    res.status(500).json({ error: "Erro ao gerar QR Code." });
+  }
+});
+
+routes.get('/', (req: Request, res: Response)=>{
+    res.statusCode = 403;
+    res.send('Acesso não permitido. Rota default não definida.');
+});
+
+routes.get("/loginPartner", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../frontend/pages/loginPartner/loginPartner.html"));
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on: ${port}`)
+})
